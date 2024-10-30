8c56f89367498961fdce43d3af8aa04151282c84
Badal-Gamit <badalgamit98@gmail.com>
2024-10-31 01:19:07 +0530

    config cors

--- client/src/pages/user/Order.jsx
+++ client/src/pages/user/Order.jsx
@@ -14,7 +14,7 @@ useEffect(() => {
 
   return (
     <div  className='mt-10' >
-    <div > {order.length<0?order?.map((o,i)=>{
+    <div > {order.length>0?order?.map((o,i)=>{
     return  <div className="relative overflow-x-auto shadow-md sm:rounded-lg" key={o._id} >
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
